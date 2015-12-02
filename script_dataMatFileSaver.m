72769e37c293fdeb95af295d42c42272bc1842d8
mranis <mranis@ucsd.edu>
2015-12-02 10:03:47 -0800

    fixed script for generating the 6.mat files

--- script_dataMatFileSaver.m
+++ script_dataMatFileSaver.m
@@ -4,25 +4,26 @@ load data_clusterseq.mat
 for i=1:3
   switch i
       case 1
-          seq=sqTrain{1}; labels=dataInfo{1};
+          seq=sqTrain{1}{1}; labels=dataInfo{1};
+          
           save data_pa_1_train.mat seq labels -mat
           save data_pa_1_train.mat seq labels -mat -append
-          seq=seqTest{1}; labels=graphInfo{1};
+          seq=seqTest{1}{1}; labels=graphInfo{1};
           save data_pa_1_test.mat seq labels -mat
           save data_pa_1_test.mat seq labels -mat -append 
           
       case 2
-          seq=sqTrain{2}; labels=dataInfo{2};
+          seq=sqTrain{2}{1}; labels=dataInfo{2};
           save data_pa_2_train.mat seq labels -mat
           save data_pa_2_train.mat seq labels -mat -append
-          seq=seqTest{2}; labels=graphInfo{2};
+          seq=seqTest{2}{1}; labels=graphInfo{2};
           save data_pa_2_test.mat seq labels -mat
           save data_pa_2_test.mat seq labels -mat -append  
       case 3
-          seq=sqTrain{3}; labels=dataInfo{3};
+          seq=sqTrain{3}{1}; labels=dataInfo{3};
           save data_pa_3_train.mat seq labels -mat
           save data_pa_3_train.mat seq labels -mat -append
-          seq=seqTest{3}; labels=graphInfo{3};
+          seq=seqTest{3}{1}; labels=graphInfo{3};
           save data_pa_3_test.mat seq labels -mat
           save data_pa_3_test.mat seq labels -mat -append      
   end % end of switch i 
